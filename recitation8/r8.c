9199415a3d23cffce5b5a44c0908759175ae7606
b-chen8 <111385765+b-chen8@users.noreply.github.com>
2025-10-29 15:44:18 -0400

    Implement add_student function for linked list
    Allocate memory for a new student and handle errors.

--- recitation8/r8.c
+++ recitation8/r8.c
@@ -8,6 +8,20 @@
 // Return: head of the list
 Student *add_student(Student **head, char *name, float grade)
 {
+    Student *new_student = (Student *)malloc(sizeof(Student));
+    if(new_student == NULL){
+      return NULL;
+    }
+
+    new_student->name = malloc(sizeof(name) + 1);
+    if(new_student->name == NULL){
+      free(new_student);
+      return NULL;
+    }
+    strcpy(new_student->name, name);
+    new_student->grade = grade;
+    new_student->next_student = *head;
+    *head = new_student;
 }
 
 // TODO: Implement
